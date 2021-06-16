889f70018f14db5fe9e57a814a7a6ddb92eda23d
Smith <pentopia11@gmail.com>
2021-06-16 01:17:09 -0700

    some aesthetic changes as well as splitting the searches up by season so that the site doesn't crash

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-function run() {
-    var result = "";
-    var span = document.getElementById("results");
-    span.textContent = "";
+function clean() {
+    document.getElementById("results").innerHTML = "";
+}
+
+function run(j) {
+    var result = "", span = document.getElementById("results"), s = document.getElementById("search").value, eps = [22, 22, 16, 22, 22, 22, 23, 23, 23, 23, 23, 23, 23, 20, 20];
     
-    var s = document.getElementById("search").value;
     s = s.toLowerCase();
-    if (s == "" || s == " ") return;
+    if (s === "" || s === " ") {
+        return;
+    }
     
     span.innerHTML += "Searching " + s + "<br>";
     
-    var eps = [22, 22, 16, 22, 22, 22, 23, 23, 23, 23, 23, 23, 23, 20, 20];
-    
-    for (var j = 1; j <= 15; j++) {
-        for (var i = 1; i <= eps[j - 1]; i++) {
-            var file = 'transcripts/' + j + '/' + i + '.txt';
+    for (var i = 1; i <= eps[j - 1]; i++) {
+        var file = 'transcripts/' + j + '/' + i + '.txt';
 
-            $.ajax({
-                url:file,
-                success: function (data) {
-                    if (data.toLowerCase().includes(s)) {
-                        span.innerHTML += (j + "." + i + "<br>");
-                    }
-                },
-                async: false
-            });
-        }
+        $.ajax({
+            url:file,
+            success: function (data) {
+                if (data.toLowerCase().includes(s)) {
+                    span.innerHTML += (j + "." + i + "<br>");
+                }
+            },
+            async: false
+        });
     }
 }
